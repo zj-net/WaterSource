17a95505eaff89ab2c68fedcf35411b9382f3900
Tristen Allen <spyboticsguy@gmail.com>
2017-02-13 18:36:09 -0500

    Rename LoginActivity -> LaunchActivity
    LoginActivity will be the actual login UI.
    Added app/build/** to .gitignore.

--- app/src/main/java/com/tristenallen/watersource/LaunchActivity.java
+++ app/src/main/java/com/tristenallen/watersource/LaunchActivity.java
@@ -3,11 +3,11 @@ package com.tristenallen.watersource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_launch);
     }
 }
