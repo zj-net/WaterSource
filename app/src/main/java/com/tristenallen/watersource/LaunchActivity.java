cd80541c38d64a3f5938211054e29dfc16ce9daa
Jahziel Villasana <jahzielv@gmail.com>
2017-02-14 12:21:34 -0500

    Added the "login" button functionality; clicking "login" takes you to the login screen.

--- app/src/main/java/com/tristenallen/watersource/LaunchActivity.java
+++ app/src/main/java/com/tristenallen/watersource/LaunchActivity.java
@@ -1,13 +1,28 @@
 package com.tristenallen.watersource;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.tristenallen.watersource.login.LoginActivity;
 
 public class LaunchActivity extends AppCompatActivity {
+    private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToLoginScreen = new Intent(LaunchActivity.this, LoginActivity.class);
+                startActivity(goToLoginScreen);
+            }
+        });
     }
 }
