4a7daff1200f92fb51b63cd0f90d9c16a39025a2
davidzxiong <davidzxiong@gmail.com>
2017-02-22 18:51:42 -0500

    populate profile with user data

--- app/src/main/java/com/tristenallen/watersource/controller/EditProfileActivity.java
+++ app/src/main/java/com/tristenallen/watersource/controller/EditProfileActivity.java
@@ -12,6 +12,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.tristenallen.watersource.R;
+import com.tristenallen.watersource.model.Model;
+import com.tristenallen.watersource.model.User;
 
 /**
  * Created by David on 2/22/17.
@@ -22,10 +24,23 @@ public class EditProfileActivity extends AppCompatActivity {
     private EditText addressField;
     private EditText emailField;
 
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        nameField = (EditText) findViewById(R.id.nameTXT);
+        addressField = (EditText) findViewById(R.id.addressTXT);
+        emailField = (EditText) findViewById(R.id.emailTXT);
+
+
+        user = Model.getCurrentUser();
+
+        nameField.setText(user.getFirstName()+user.getLastName());
+        addressField.setText(user.getAddress());
+        emailField.setText(user.getEmail());
     }
 
 }
