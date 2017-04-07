0189b487a433742dafe3003531ace31808346d5f
davidzxiong <davidzxiong@gmail.com>
2017-04-07 19:37:49 -0400

    unused import

--- app/src/main/java/com/tristenallen/watersource/controller/MainActivity.java
+++ app/src/main/java/com/tristenallen/watersource/controller/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
