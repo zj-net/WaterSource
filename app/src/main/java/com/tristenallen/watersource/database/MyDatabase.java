ad4313e20c2c4d8078187f28be63dd7818cc9907
Tristen Allen <spyboticsguy@gmail.com>
2017-04-10 23:42:30 -0400

    Some more database changes.

--- app/src/main/java/com/tristenallen/watersource/database/MyDatabase.java
+++ app/src/main/java/com/tristenallen/watersource/database/MyDatabase.java
@@ -16,78 +16,24 @@ import com.tristenallen.watersource.model.User;
  * The database implementation class.
  */
 
-public class MyDatabase extends SQLiteOpenHelper implements DataSource {
-    // Database table column names
-    private static final String TABLE_NAME = "User";
+public class MyDatabase implements DataSource {
 
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_PASSWORD = "password";
-    private static final String COLUMN_EMAIL = "email";
-    private static final String COLUMN_ROLE = "role";
-    private static final String COLUMN_ADDRESS = "address";
-    private static final String COLUMN_TITLE = "title";
-    private static final String COLUMN_LASTNAME = "lastname";
-    private static final String COLUMN_FIRSTNAME = "firstname";
-
-    private static final String[] allColumns = { COLUMN_ID, COLUMN_PASSWORD,
-            COLUMN_EMAIL, COLUMN_ROLE, COLUMN_ADDRESS, COLUMN_TITLE,
-            COLUMN_LASTNAME, COLUMN_FIRSTNAME};
-
-    private static final int ID_NUMBER = 0;
-    private static final int EMAIL_NUMBER = 2;
-    private static final int ROLE_NUMBER = 3;
-    private static final int LASTNAME_NUMBER = 6;
-    private static final int FIRSTNAME_NUMBER = 7;
-
-    // Database creation SQL statement
-    private static final String DATABASE_CREATE = "create table "
-            + TABLE_NAME
-            + "("
-            + COLUMN_ID + " integer primary key autoincrement, "
-            + COLUMN_PASSWORD + " text not null,"
-            + COLUMN_EMAIL + " text not null,"
-            + COLUMN_ROLE + " text not null,"
-            + COLUMN_ADDRESS + " text not null,"
-            + COLUMN_TITLE + " text not null,"
-            + COLUMN_LASTNAME + " text not null,"
-            + COLUMN_FIRSTNAME + " text not null"
-            + ");";
-
-    private static final String DATABASE_NAME = "student.db";
-    private static final int DATABASE_VERSION = 1;
+    private final MyDatabaseHelper openHelper;
     private SQLiteDatabase database;
-
+    private static final String[] allColumns = {MyDatabaseHelper.COLUMN_ID, MyDatabaseHelper.COLUMN_PASSWORD,
+            MyDatabaseHelper.COLUMN_EMAIL, MyDatabaseHelper.COLUMN_ROLE, MyDatabaseHelper.COLUMN_ADDRESS,
+            MyDatabaseHelper.COLUMN_TITLE, MyDatabaseHelper.COLUMN_LASTNAME, MyDatabaseHelper.COLUMN_FIRSTNAME};
     /**
      * constructor method to create a database
      * @param context Context the activity context
      */
     public MyDatabase(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        openHelper = new MyDatabaseHelper(context);
         open();
     }
 
     private void open() throws SQLException {
-        database = getWritableDatabase();
-    }
-
-    /**
-     * method for create the database
-     */
-    private void onCreate() {
-        database.execSQL(DATABASE_CREATE);
-    }
-
-    /**
-     * method update db from old version to new version
-     * @param oldVersion int old version number
-     * @param newVersion int new version number
-     */
-    private void onUpgrade(int oldVersion, int newVersion) {
-        Log.w(MyDatabase.class.getName(), "Upgrading database from version "
-                + oldVersion + " to " + newVersion
-                + ", which will destroy all old data");
-        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate();
+        database = openHelper.getWritableDatabase();
     }
 
     @SuppressWarnings("FeatureEnvy") // necessary as User is a data holder and we are converting to a DB format
@@ -95,22 +41,22 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
     public void createUser(int id, String password, String address, String title, User user) {
         ContentValues values = new ContentValues();
         AuthLevel role = user.getRole();
-        values.put(COLUMN_ID, id);
-        values.put(COLUMN_PASSWORD, password);
-        values.put(COLUMN_EMAIL, user.getEmail());
-        values.put(COLUMN_ROLE, role.toString());
-        values.put(COLUMN_ADDRESS, address);
-        values.put(COLUMN_TITLE, title);
-        values.put(COLUMN_LASTNAME, user.getLastName());
-        values.put(COLUMN_FIRSTNAME, user.getFirstName());
+        values.put(MyDatabaseHelper.COLUMN_ID, id);
+        values.put(MyDatabaseHelper.COLUMN_PASSWORD, password);
+        values.put(MyDatabaseHelper.COLUMN_EMAIL, user.getEmail());
+        values.put(MyDatabaseHelper.COLUMN_ROLE, role.toString());
+        values.put(MyDatabaseHelper.COLUMN_ADDRESS, address);
+        values.put(MyDatabaseHelper.COLUMN_TITLE, title);
+        values.put(MyDatabaseHelper.COLUMN_LASTNAME, user.getLastName());
+        values.put(MyDatabaseHelper.COLUMN_FIRSTNAME, user.getFirstName());
 
-        long insertId = database.insert(TABLE_NAME, null,
+        long insertId = database.insert(MyDatabaseHelper.TABLE_NAME, null,
                 values);
 
         Log.d("APP", "Inserted User: " + insertId);
 
-        Cursor cursor = database.query(TABLE_NAME,
-                allColumns, COLUMN_ID + " = " + insertId, null,
+        Cursor cursor = database.query(MyDatabaseHelper.TABLE_NAME,
+                allColumns, MyDatabaseHelper.COLUMN_ID + " = " + insertId, null,
                 null, null, null);
 
         cursor.moveToFirst();
@@ -119,18 +65,19 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
     }
 
     private static User cursorToUser(Cursor cursor) {
-        String email = cursor.getString(EMAIL_NUMBER);
-        String roleString = cursor.getString(ROLE_NUMBER);
+        String email = cursor.getString(MyDatabaseHelper.EMAIL_NUMBER);
+        String roleString = cursor.getString(MyDatabaseHelper.ROLE_NUMBER);
         AuthLevel role = AuthLevel.valueOf(roleString.toUpperCase());
-        String lastName = cursor.getString(LASTNAME_NUMBER);
-        String firstName = cursor.getString(FIRSTNAME_NUMBER);
+        String lastName = cursor.getString(MyDatabaseHelper.LASTNAME_NUMBER);
+        String firstName = cursor.getString(MyDatabaseHelper.FIRSTNAME_NUMBER);
         return new User(email, role, lastName, firstName);
     }
 
 
     @Override
     public boolean checkEmail(String email) {
-        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_EMAIL + " ='" + email + "'";
+        String query = "SELECT * FROM " + MyDatabaseHelper.TABLE_NAME + " WHERE " + MyDatabaseHelper.COLUMN_EMAIL
+                + " ='" + email + "'";
 
         Cursor cursor = database.rawQuery(query,null);
         Log.d("checkEmail******",cursor.toString());
@@ -145,8 +92,8 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
 
     @Override
     public boolean validate(String email, String password) {
-        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_EMAIL + " ='"
-                + email + "' AND " + COLUMN_PASSWORD + " ='" + password + "'";
+        String query = "SELECT * FROM " + MyDatabaseHelper.TABLE_NAME + " WHERE " + MyDatabaseHelper.COLUMN_EMAIL
+                + " ='" + email + "' AND " + MyDatabaseHelper.COLUMN_PASSWORD + " ='" + password + "'";
 
         Cursor cursor = database.rawQuery(query,null);
 
@@ -160,13 +107,14 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
 
     @Override
     public int getIDbyEmail(String email) {
-        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_EMAIL + " ='" + email + "'";
+        String query = "SELECT * FROM " + MyDatabaseHelper.TABLE_NAME + " WHERE " + MyDatabaseHelper.COLUMN_EMAIL
+                + " ='" + email + "'";
 
         Cursor cursor = database.rawQuery(query,null);
 
         if (cursor.getCount()>0) {
             cursor.moveToFirst();
-            int id = cursor.getInt(ID_NUMBER);
+            int id = cursor.getInt(MyDatabaseHelper.ID_NUMBER);
             cursor.close();
             return id;
         }
@@ -179,7 +127,8 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
 
     @Override
     public User getUserByID(int id) {
-        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = " + id;
+        String query = "SELECT * FROM " + MyDatabaseHelper.TABLE_NAME + " WHERE " + MyDatabaseHelper.COLUMN_ID
+                + " = " + id;
 
         Cursor  cursor = database.rawQuery(query,null);
 
@@ -198,7 +147,7 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
 
     @Override
     public int getUserCount() {
-        String query = "SELECT * FROM " + TABLE_NAME;
+        String query = "SELECT * FROM " + MyDatabaseHelper.TABLE_NAME;
 
         Cursor  cursor = database.rawQuery(query,null);
 
@@ -208,15 +157,59 @@ public class MyDatabase extends SQLiteOpenHelper implements DataSource {
         return row_count;
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase database) {
-        onCreate();
-    }
+    private final class MyDatabaseHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "student.db";
+        private static final int DATABASE_VERSION = 1;
+        // Database table column names
+        private static final String TABLE_NAME = "User";
 
-    @Override
-    public void onUpgrade(SQLiteDatabase database, int oldVersion,
-                          int newVersion) {
-        onUpgrade(oldVersion, newVersion);
+        private static final String COLUMN_ID = "id";
+        private static final String COLUMN_PASSWORD = "password";
+        private static final String COLUMN_EMAIL = "email";
+        private static final String COLUMN_ROLE = "role";
+        private static final String COLUMN_ADDRESS = "address";
+        private static final String COLUMN_TITLE = "title";
+        private static final String COLUMN_LASTNAME = "lastname";
+        private static final String COLUMN_FIRSTNAME = "firstname";
+
+        private static final int ID_NUMBER = 0;
+        private static final int EMAIL_NUMBER = 2;
+        private static final int ROLE_NUMBER = 3;
+        private static final int LASTNAME_NUMBER = 6;
+        private static final int FIRSTNAME_NUMBER = 7;
+
+        // Database creation SQL statement
+        private static final String DATABASE_CREATE = "create table "
+                + TABLE_NAME
+                + "("
+                + COLUMN_ID + " integer primary key autoincrement, "
+                + COLUMN_PASSWORD + " text not null,"
+                + COLUMN_EMAIL + " text not null,"
+                + COLUMN_ROLE + " text not null,"
+                + COLUMN_ADDRESS + " text not null,"
+                + COLUMN_TITLE + " text not null,"
+                + COLUMN_LASTNAME + " text not null,"
+                + COLUMN_FIRSTNAME + " text not null"
+                + ");";
+
+        private MyDatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase database) {
+            database.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+            Log.w(MyDatabaseHelper.class.getName(), "Upgrading database from version "
+                    + oldVersion + " to " + newVersion
+                    + ", which will destroy all old data");
+            database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(database);
+        }
+
     }
 
 }
